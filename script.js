3a8ba21c88c445e8411e4f66afa55b2e0749e624
Goran Ivkić <65123282+gogifswd10@users.noreply.github.com>
2020-05-27 12:00:31 +0200

    basic 1, 2 and 3

---
+++ script.js
@@ -0,0 +1,32 @@
+//BASIC 1
+
+/*function crystalGazer(numberOfChildren, partnersName, geographicLocation, jobTitle) {
+	return numberOfChildren - partnersName - geographicLocation - jobTitle;
+}
+
+	var	numberOfChildren =(1, 3);
+	var partnersName = ("Peter", "John", "Mike");
+	var geographicLocation = ("Paris", "London", "Vienna");
+	var jobTitle = ("Developer", "Asshole", "Doc", "Pornstar");
+	document.write("You will be " + jobTitle + " in " + geographicLocation + " and married to " + partnersName + " with " + numberOfChildren + " children.");
+*/
+
+//BASIC 2 BASIC 3
+
+
+/*function ageCalculator(date, birthYear) {
+		return  date - birthYear;
+	}	
+	var date = new Date().getFullYear();
+	var birthYear = 1981;
+	var age = ageCalculator(date, birthYear);
+	var age2 = age-1;
+
+	document.write("You are either " + age + " or " + age2);
+*/
+
+
+
+
+
+
